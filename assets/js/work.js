9e2f308d4371957bd31b3538b9f47d8bae449d85
santamaria000 <shomacco@gmail.com>
2026-02-25 03:13:42 +0900

    変更内容を一言

--- assets/js/work.js
+++ assets/js/work.js
@@ -4,109 +4,107 @@
 window.WORKS = {
   kanzo: {
     title: "She Her Her Hers - KANZO",
-    video: "https://www.youtube.com/embed/BXSi5kvypRA?autoplay=1&rel=0",
+    video: "https://www.youtube.com/embed/BXSi5kvypRA?autoplay=1&rel=0&enablejsapi=1",
     credits: []
   },
   misfits: {
     title: "ドレスコーズ 「ミスフィッツ」",
-    video: "https://www.youtube.com/embed/h9MxX39Di_4?autoplay=1&rel=0",
+    video: "https://www.youtube.com/embed/h9MxX39Di_4?autoplay=1&rel=0&enablejsapi=1",
     credits: []
   },
   dokuhaku: {
     title: "山本大斗 - 独白",
-    video: "https://www.youtube.com/embed/JoDadfbpA0k?autoplay=1&rel=0",
+    video: "https://www.youtube.com/embed/JoDadfbpA0k?autoplay=1&rel=0&enablejsapi=1",
     credits: []
   },
   higher: {
     title: "luvis - Higher",
-    video: "https://www.youtube.com/embed/s7zyqdo12S4?autoplay=1&rel=0",
+    video: "https://www.youtube.com/embed/s7zyqdo12S4?autoplay=1&rel=0&enablejsapi=1",
     credits: []
   },
   all_my_life: {
     title: "清 竜人 - All My Life",
-    video: "https://www.youtube.com/embed/vOEBTZCBKDM?autoplay=1&rel=0",
+    video: "https://www.youtube.com/embed/vOEBTZCBKDM?autoplay=1&rel=0&enablejsapi=1",
     credits: []
   },
   tokyo_mushuku: {
     title: "BLYY - 東京無宿",
-    video: "https://www.youtube.com/embed/FK2_PttkHeo?autoplay=1&rel=0",
+    video: "https://www.youtube.com/embed/FK2_PttkHeo?autoplay=1&rel=0&enablejsapi=1",
     credits: []
   },
   revenge: {
     title: "ShowyVICTOR - REVENGE",
-    video: "https://www.youtube.com/embed/WHOK0bwvM8c?autoplay=1&rel=0",
+    video: "https://www.youtube.com/embed/WHOK0bwvM8c?autoplay=1&rel=0&enablejsapi=1",
     credits: []
   },
   too_high: {
     title: "ゆるふわギャング - Too High",
-    video: "https://www.youtube.com/embed/Jj5YTAw6FU0?autoplay=1&rel=0",
+    video: "https://www.youtube.com/embed/Jj5YTAw6FU0?autoplay=1&rel=0&enablejsapi=1",
     credits: []
   },
   new_world_believer: {
     title: "鯱 - New World Believer",
-    video: "https://www.youtube.com/embed/1R6kZCws3uw?autoplay=1&rel=0",
+    video: "https://www.youtube.com/embed/1R6kZCws3uw?autoplay=1&rel=0&enablejsapi=1",
     credits: []
   },
   money_from_the_dirt: {
     title: "MEZZ - Money From The Dirt",
-    video: "https://www.youtube.com/embed/atf9BOfnIKk?autoplay=1&rel=0",
+    video: "https://www.youtube.com/embed/atf9BOfnIKk?autoplay=1&rel=0&enablejsapi=1",
     credits: []
   },
   supersonic: {
     title: "鋭児 - $uper$onic",
-    video: "https://www.youtube.com/embed/5FOkDlPUuZo?autoplay=1&rel=0",
+    video: "https://www.youtube.com/embed/5FOkDlPUuZo?autoplay=1&rel=0&enablejsapi=1",
     credits: []
   },
   imaginary_line: {
     title: "She Her Her Hers - Imaginary line",
-    video: "https://www.youtube.com/embed/yiqnsvDw-3Y?autoplay=1&rel=0",
+    video: "https://www.youtube.com/embed/yiqnsvDw-3Y?autoplay=1&rel=0&enablejsapi=1",
     credits: []
   },
   zion: {
     title: "鋭児 - Zion",
-    video: "https://www.youtube.com/embed/EBvSHwMDPQ4?autoplay=1&rel=0",
+    video: "https://www.youtube.com/embed/EBvSHwMDPQ4?autoplay=1&rel=0&enablejsapi=1",
     credits: []
   },
   ignite: {
     title: "鋭児 - IGNITE",
-    video: "https://www.youtube.com/embed/UyRKuMLxEo8?autoplay=1&rel=0",
+    video: "https://www.youtube.com/embed/UyRKuMLxEo8?autoplay=1&rel=0&enablejsapi=1",
     credits: []
   },
   sophnet_end_martens: {
     title: "SOPHNET. END. Dr. Martens",
-    video: "https://www.youtube.com/embed/oolk2-r-100?autoplay=1&rel=0",
+    video: "https://www.youtube.com/embed/oolk2-r-100?autoplay=1&rel=0&enablejsapi=1",
     credits: []
   },
   silent_madness: {
     title: "NAGAN SERVER 「SILENT MADNESS」",
-    video: "https://www.youtube.com/embed/TZAbbcR-Mqg?autoplay=1&rel=0",
+    video: "https://www.youtube.com/embed/TZAbbcR-Mqg?autoplay=1&rel=0&enablejsapi=1",
     credits: []
   },
   kaizodo: {
     title: "amazarashi『世界の解像度』",
-    video: "https://www.youtube.com/embed/iwGogQZe-AQ?autoplay=1&rel=0",
+    video: "https://www.youtube.com/embed/iwGogQZe-AQ?autoplay=1&rel=0&enablejsapi=1",
     credits: []
   },
   shisoukaseki: {
     title: "saccharin - Shisoukaseki",
-    video: "https://www.youtube.com/embed/iD9hfjvpms0?autoplay=1&rel=0",
+    video: "https://www.youtube.com/embed/iD9hfjvpms0?autoplay=1&rel=0&enablejsapi=1",
     credits: []
   },
   madras_night_part2: {
     title: "MADRAS NIGHT PART 2",
-    video: "https://www.youtube.com/embed/joPPncT8z14?autoplay=1&rel=0",
+    video: "https://www.youtube.com/embed/joPPncT8z14?autoplay=1&rel=0&enablejsapi=1",
     credits: []
   },
   ginga: {
     title: "銀河",
-    video: "https://www.youtube.com/embed/kIMNUqJusnQ?autoplay=1&rel=0",
+    video: "https://www.youtube.com/embed/kIMNUqJusnQ?autoplay=1&rel=0&enablejsapi=1",
     credits: []
   },
-
-  // creditsありサンプル
   silent_city: {
     title: "Silent City",
-    video: "https://www.youtube.com/embed/vOEBTZCBKDM?autoplay=1&rel=0",
+    video: "https://www.youtube.com/embed/vOEBTZCBKDM?autoplay=1&rel=0&enablejsapi=1",
     credits: [
       { role: "Director", name: "Oliver Bennett" },
       { role: "Producer", name: "Sienna Ward" },
