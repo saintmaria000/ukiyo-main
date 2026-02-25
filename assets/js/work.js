10071730d18720872778004d72b3b30b5970f8f9
santamaria000 <shomacco@gmail.com>
2026-02-25 14:32:14 +0900

    変更内容を一言

--- assets/js/work.js
+++ assets/js/work.js
@@ -5,82 +5,404 @@ window.WORKS = {
   kanzo: {
     title: "She Her Her Hers - KANZO",
     video: "https://www.youtube.com/embed/BXSi5kvypRA?autoplay=1&rel=0&enablejsapi=1",
-    credits: []
+    credits: [
+        { role: "Director", name: "ZECIN" },
+        { role: "Producer", name: "Matias Maehama" },
+
+        { role: "Programming", name: "Hiroyasu Takahashi" },
+        { role: "Drums", name: "Taiki Matsuura" },
+        { role: "Violin", name: "Moemi Harada" },
+
+        { role: "Music", name: "Hiroyasu Takahashi" },
+        { role: "Lyrics", name: "Taiki Matsuura" },
+        { role: "Music Arrangement & Production", name: "She Her Her Hers" },
+
+        { role: "Drums Recording", name: "Makoto Hoshino" },
+        { role: "Drums Recorded At", name: "Studio GOATEE" },
+
+        { role: "Mixing & Mastering", name: "Makoto Hoshino" }
+    ]
   },
   misfits: {
     title: "ドレスコーズ 「ミスフィッツ」",
     video: "https://www.youtube.com/embed/h9MxX39Di_4?autoplay=1&rel=0&enablejsapi=1",
-    credits: []
+    credits: [
+        { role: "Director", name: "Matías Maehama" },
+        { role: "Director of Photography", name: "Ryosuke Sato" },
+        { role: "Assistant Camera", name: "Taiga" },
+
+        { role: "Gaffer", name: "Wataru Sugimura" },
+        { role: "Lighting Assistant", name: "Yuto Fukata" },
+        { role: "Lighting Assistant", name: "Kadowaki Yuuki" },
+
+        { role: "Stylist", name: "Koji Taura" },
+        { role: "Hair & Make-Up", name: "Rin Yokoyama" },
+        { role: "Hair & Make-Up Assistant", name: "Kazuma Sakano" },
+
+        { role: "Offline Editor", name: "Matias Maehama" },
+        { role: "Offline Editor", name: "nave" },
+
+        { role: "Production Manager", name: "Shota Watanabe" },
+        { role: "Prop", name: "Shota Watanabe" },
+
+        { role: "Production Assistant", name: "Syunsuke Soga" },
+
+        { role: "Production", name: "Ukiyo Film" }
+    ]
   },
   dokuhaku: {
     title: "山本大斗 - 独白",
     video: "https://www.youtube.com/embed/JoDadfbpA0k?autoplay=1&rel=0&enablejsapi=1",
-    credits: []
+    credits: [
+        { role: "Cast", name: "Aki Kigoshi" },
+        { role: "Cast", name: "Yamada Django" },
+
+        { role: "Director", name: "Matías Maehama" },
+        { role: "Director of Photography", name: "Ryosuke Sato" },
+        { role: "Assistant Camera", name: "Taiga" },
+
+        { role: "Best Boy Grip", name: "Shota Watanabe" },
+
+        { role: "Stylist", name: "Ko Nakagawara" },
+        { role: "Stylist Assistant", name: "Raiki Shintani" },
+
+        { role: "Hair & Make-Up", name: "Kei Kouda" },
+
+        { role: "Offline Editor", name: "Matias Maehama" },
+
+        { role: "Colorist", name: "Haimei" },
+        { role: "Colorist Assistant", name: "Colorín" },
+
+        { role: "Location Coordinator", name: "Hiroki Nakazawa" },
+
+        { role: "Car", name: "Tamon" },
+
+        { role: "Production Assistant", name: "Ryu Watanabe" },
+
+        { role: "Special Thanks", name: "mapleheim" },
+
+        { role: "Production", name: "Ukiyo Film" }
+    ]
   },
   higher: {
     title: "luvis - Higher",
     video: "https://www.youtube.com/embed/s7zyqdo12S4?autoplay=1&rel=0&enablejsapi=1",
-    credits: []
+    credits: [
+        { role: "Director", name: "ZECIN" },
+        { role: "Producer", name: "Matias Maehama" },
+
+        { role: "Vocal / Chorus / Bass / Synthesizer / Programming", name: "Hiroyasu Takahashi" },
+        { role: "Drums", name: "Taiki Matsuura" },
+        { role: "Violin", name: "Moemi Harada" },
+
+        { role: "Music Written By", name: "Hiroyasu Takahashi" },
+        { role: "Lyrics Written By", name: "Taiki Matsuura" },
+        { role: "Music Arranged & Produced By", name: "She Her Her Hers" },
+
+        { role: "Drums Recorded By", name: "Makoto Hoshino" },
+        { role: "Drums Recorded At", name: "Studio GOATEE" },
+
+        { role: "Mixed & Mastered By", name: "Makoto Hoshino" }
+    ]
   },
   all_my_life: {
     title: "清 竜人 - All My Life",
     video: "https://www.youtube.com/embed/vOEBTZCBKDM?autoplay=1&rel=0&enablejsapi=1",
-    credits: []
+    credits: [
+        { role: "Artist", name: "SORA" },
+        { role: "Artist", name: "Riku Tanaka" },
+
+        { role: "Director", name: "Matías Maehama" },
+        { role: "Director of Photography", name: "Ryosuke Sato" },
+
+        { role: "Stylist", name: "Ko Nakagawara" },
+        { role: "Hair & Make-Up", name: "Kei Kouda" },
+
+        { role: "Online Editor", name: "Kosuke Nakajima" },
+        { role: "Colorist", name: "Oudai Kojima" },
+
+        { role: "Location Coordinator", name: "Hiroki Nakazawa" },
+        { role: "Production Manager", name: "Shota Watanabe" },
+        { role: "Production Assistant", name: "Niiyama Shun" },
+
+        { role: "Bikeman", name: "Yuki Koichi" }
+    ]
   },
   tokyo_mushuku: {
     title: "BLYY - 東京無宿",
     video: "https://www.youtube.com/embed/FK2_PttkHeo?autoplay=1&rel=0&enablejsapi=1",
-    credits: []
+    credits: [
+        { role: "Director", name: "Hideto Hotta" },
+        { role: "Cinematographer", name: "Ryosuke Sato" },
+
+        { role: "Prop Stylist", name: "Sui Nakane" },
+
+        { role: "Editor", name: "Hideto Hotta" },
+        { role: "Colorist", name: "Yoshiyuki Nishida" },
+
+        { role: "Title Design", name: "JELLY FLASH" },
+        { role: "Motion Graphics", name: "Una Abe" },
+
+        { role: "Sound Mix", name: "Tatsuya Kishimoto" },
+
+        { role: "Photographer", name: "Toshiaki Kitaoka" },
+        { role: "Assistant Photographer", name: "Naoto Kawatake" },
+
+        { role: "Stylist", name: "Daisuke Deguchi" },
+
+        { role: "Hair & Make-Up", name: "Hiroki Kato" },
+
+        { role: "Production Manager", name: "Shota Watanabe" },
+        { role: "Production Manager", name: "YOICHIRO" },
+        { role: "Production Manager", name: "Arashi Ishikawa" },
+
+        { role: "Assistant Director", name: "YUNA" },
+
+        { role: "Producer", name: "Mahtias Maehama" },
+
+        { role: "Lyrics Translation", name: "Aya Kinami (STARBASE.INC)" },
+
+        { role: "Special Thanks", name: "Rintaro Ito" }
+    ]
   },
   revenge: {
     title: "ShowyVICTOR - REVENGE",
     video: "https://www.youtube.com/embed/WHOK0bwvM8c?autoplay=1&rel=0&enablejsapi=1",
-    credits: []
+    credits: [
+        { role: "Director", name: "Matias Maehama" },
+        { role: "Director of Photography", name: "Ryosuke Sato" },
+        { role: "Gaffer", name: "Wataru Sugimura" },
+
+        { role: "Vibes Leader", name: "Kazuma Asami" },
+
+        { role: "Title Design", name: "mad.O" },
+        { role: "Colorist", name: "Max Golomidov" },
+
+        { role: "Location", name: "checkmark-FURUGIYA-" },
+
+        { role: "Producer", name: "Shotaro Ito" },
+        { role: "Creative Production", name: "Skid" },
+
+        { role: "Special Thanks", name: "GUILLO" }
+    ]
   },
   too_high: {
     title: "ゆるふわギャング - Too High",
     video: "https://www.youtube.com/embed/Jj5YTAw6FU0?autoplay=1&rel=0&enablejsapi=1",
-    credits: []
+    credits: [
+        { role: "Director", name: "Hideto Hotta" },
+        { role: "Cinematographer", name: "Yuki Ono" },
+        { role: "Assistant Camera", name: "Kazuya Nakamura" },
+        { role: "Drone Operator", name: "Daisuke Maeda" },
+
+        { role: "Gaffer", name: "Wataru Sugimura" },
+
+        { role: "Artwork", name: "SRTMWORKS" },
+
+        { role: "Stylist", name: "NUGA (DENSE)" },
+        { role: "Clothing", name: "contena vintage" },
+        { role: "Clothing", name: "Oopsclothing" },
+        { role: "Clothing", name: "DENSE TOKYO" },
+        { role: "Clothing", name: "RADD LOUNGE" },
+
+        { role: "Hair", name: "Aki" },
+        { role: "Make-Up", name: "TORI" },
+
+        { role: "Motion Graphics", name: "Ryutaro Kobayashi" },
+        { role: "Compositing", name: "Una Abe" },
+
+        { role: "Production Manager", name: "Arashi Ishikawa" },
+        { role: "Producer", name: "Matias Maehama" },
+
+        { role: "Special Thanks", name: "Aiko Nishi" },
+        { role: "Special Thanks", name: "Ryo Ito" }
+    ]
   },
   new_world_believer: {
     title: "鯱 - New World Believer",
     video: "https://www.youtube.com/embed/1R6kZCws3uw?autoplay=1&rel=0&enablejsapi=1",
-    credits: []
+    credits: [
+
+        { role: "Director", name: "Matias Maehama (SKID)" },
+        { role: "Producer", name: "Shotaro Ito (SKID)" },
+        { role: "Producer", name: "YOICHIRO (SKID)" },
+
+        { role: "Director of Photography", name: "Ryosuke Sato (SKID)" },
+        { role: "Gaffer", name: "Wataru Sugimura" },
+
+        { role: "Assistant Camera", name: "Sei Shimu" },
+        { role: "Lighting Assistant", name: "You Kumekawa" },
+
+        { role: "Art Director", name: "Sui (SKID)" },
+        { role: "Paint", name: "Sakurako" },
+
+        { role: "Stylist", name: "Kazuma Asami (SKID)" },
+        { role: "Hair & Make-Up", name: "Kei Kouda (SKID)" },
+
+        { role: "Logo Design", name: "Mad O (SKID)" },
+        { role: "Colorist", name: "Yoshiyuki Nishida" },
+
+        { role: "Production Manager", name: "TAIGA (SKID)" },
+        { role: "Production Manager", name: "Kei Ukemori (SKID)" },
+        { role: "Production Manager", name: "Ryunosuke Ito" },
+        { role: "Production Manager", name: "Leo Youlagi" },
+
+        { role: "Production", name: "SKID" }
+    ]
   },
   money_from_the_dirt: {
     title: "MEZZ - Money From The Dirt",
     video: "https://www.youtube.com/embed/atf9BOfnIKk?autoplay=1&rel=0&enablejsapi=1",
-    credits: []
+    credits: [
+        { role: "Director", name: "Matias Maehama" },
+        { role: "Director of Photography", name: "Ryosuke Sato" },
+        { role: "Gaffer", name: "Ogura Hajime" },
+
+        { role: "Logo Design", name: "Mad O" },
+        { role: "Stylist", name: "Kazuma Asami" },
+
+        { role: "Producer", name: "Shotaro Ito" },
+
+        { role: "Creative Production", name: "Skid" },
+
+        { role: "Music Label", name: "+809 (WARNER MUSIC JAPAN)" },
+
+        { role: "Special Thanks", name: "Yuki Defago" },
+        { role: "Special Thanks", name: "Barbara" },
+        { role: "Special Thanks", name: "DISCODEVIL" }
+    ]
   },
   supersonic: {
     title: "鋭児 - $uper$onic",
     video: "https://www.youtube.com/embed/5FOkDlPUuZo?autoplay=1&rel=0&enablejsapi=1",
-    credits: []
+    credits: [
+        { role: "Director", name: "Matias Maehama" },
+        { role: "Director of Photography", name: "Ryosuke Sato" },
+        { role: "Second Assistant Camera", name: "Arashi Ishikawa" },
+
+        { role: "Production Manager", name: "YOICHIRO" },
+        { role: "Producer", name: "Shotaro Ito" },
+        { role: "Production", name: "Skid" },
+    ]
   },
   imaginary_line: {
     title: "She Her Her Hers - Imaginary line",
     video: "https://www.youtube.com/embed/yiqnsvDw-3Y?autoplay=1&rel=0&enablejsapi=1",
-    credits: []
+    credits: [
+        { role: "Animation & Director", name: "ZECIN" },
+
+        { role: "Creative Director", name: "Matias Maehama" },
+
+        { role: "Producer", name: "Shotaro Ito" },
+
+        { role: "Production", name: "Skid" }
+    ]
   },
   zion: {
     title: "鋭児 - Zion",
     video: "https://www.youtube.com/embed/EBvSHwMDPQ4?autoplay=1&rel=0&enablejsapi=1",
-    credits: []
+    credits: [
+        { role: "Director", name: "Ryutaro Kobayashi (SKID)" },
+        { role: "Producer", name: "Matias Maehama (SKID)" },
+        { role: "Creative Director", name: "TAIGA (SKID)" },
+
+        { role: "Director of Photography", name: "Ryosuke Sato" },
+        { role: "Gaffer", name: "Wataru Sugimura" },
+
+        { role: "Stylist", name: "Kazuma Asami (SKID)" },
+        { role: "Hair & Make-Up", name: "Kei Kouda (SKID)" },
+
+        { role: "Superman", name: "YOICHIRO (SKID)" },
+
+        { role: "Executive Producer", name: "Naoyuki Chikatani (To U Music Inc.)" },
+
+        { role: "Production", name: "SKID" }
+    ]
   },
   ignite: {
     title: "鋭児 - IGNITE",
     video: "https://www.youtube.com/embed/UyRKuMLxEo8?autoplay=1&rel=0&enablejsapi=1",
-    credits: []
+    credits: [
+        { role: "Dancer", name: "Dr.Pay" },
+
+        { role: "Director", name: "Matias Maehama (SKID)" },
+        { role: "Director of Photography", name: "Ryosuke Sato (SKID)" },
+
+        { role: "Special Effects Make-Up", name: "Lala Sagara" },
+
+        { role: "Art Director", name: "Sui" },
+
+        { role: "Title Design", name: "mad O (SKID)" },
+
+        { role: "Producer", name: "Shotaro Ito (SKID)" },
+
+        { role: "Production", name: "Skid" }
+    ]
   },
   sophnet_end_martens: {
     title: "SOPHNET. END. Dr. Martens",
     video: "https://www.youtube.com/embed/oolk2-r-100?autoplay=1&rel=0&enablejsapi=1",
-    credits: []
+    credits: [
+        { role: "Written & Produced", name: "Matias Maehama (SKID)" },
+
+        { role: "Director", name: "TAIGA (SKID)" },
+
+        { role: "Photography", name: "Ryoske Sato" },
+        { role: "Steadicam Operator", name: "Genki Hidaka" },
+
+        { role: "Gaffer", name: "Sugimura Wataru" },
+        { role: "Lighting Assistant", name: "Nakamura Ayumi" },
+
+        { role: "Stylist", name: "Shuhei Yoshida" },
+        { role: "Hair & Make-Up", name: "Kei Kouda (SKID)" },
+
+        { role: "Music", name: "Dr.Pay (ASILIS, SKID)" },
+
+        { role: "Production Manager", name: "YOICHIRO (SKID)" },
+
+        { role: "Executive Producer", name: "SOPH.co.,ltd." },
+
+        { role: "Production", name: "SKID" }
+    ]
   },
   silent_madness: {
     title: "NAGAN SERVER 「SILENT MADNESS」",
     video: "https://www.youtube.com/embed/TZAbbcR-Mqg?autoplay=1&rel=0&enablejsapi=1",
-    credits: []
+    credits: [
+        { role: "Beats", name: "dhrma" },
+
+        { role: "Cast", name: "Kyoichi Mikuriya (SKID)" },
+
+        { role: "Director", name: "TAIGA (SKID)" },
+        { role: "Producer", name: "Matias Maehama (SKID)" },
+
+        { role: "Director of Photography", name: "Ryoske Sato" },
+        { role: "First Assistant Camera", name: "Sei Shimura" },
+
+        { role: "Gaffer", name: "Sugimura Wataru" },
+        { role: "Lighting Assistant", name: "Ryo Ueda" },
+
+        { role: "Wall Paint & Props Design", name: "Sui (SKID)" },
+        { role: "Wall Paint & Props Design", name: "Aiku Kurotaki (SKID)" },
+        { role: "Wall Paint & Props Design", name: "Rio Kuroda" },
+
+        { role: "Stylist", name: "Daisuke Deguchi" },
+        { role: "Stylist Assistant", name: "Anji Fujimoto" },
+
+        { role: "Costume", name: "TIGHTBOOTH PRODUCTION" },
+        { role: "Costume", name: "NICHOLAS DALEY" },
+        { role: "Costume", name: "ENCOMING" },
+        { role: "Costume", name: "Mecha" },
+        { role: "Costume", name: "黒BENZ" },
+        { role: "Costume", name: "川" },
+
+        { role: "Production Manager", name: "YOICHIRO (SKID)" },
+        { role: "Production Assistant", name: "Kazuma Asami (SKID)" },
+
+        { role: "Production", name: "SKID" },
+
+        { role: "Management", name: "IN THE FLIGHT" }
+    ]
   },
   kaizodo: {
     title: "amazarashi『世界の解像度』",
@@ -90,32 +412,101 @@ window.WORKS = {
   shisoukaseki: {
     title: "saccharin - Shisoukaseki",
     video: "https://www.youtube.com/embed/iD9hfjvpms0?autoplay=1&rel=0&enablejsapi=1",
-    credits: []
+    credits: [
+        { role: "Cast", name: "Shouhei Nakabayashi" },
+        { role: "Cast", name: "Kyoichi Mikuriya" },
+        { role: "Cast", name: "Arashi" },
+        { role: "Cast", name: "Kohey Nishi" },
+        { role: "Cast", name: "Santamaria" },
+
+        { role: "Director", name: "Skid" },
+        { role: "Director", name: "TAIGA" },
+
+        { role: "Director of Photography", name: "Ryosuke Sato (SKID)" },
+        { role: "Gaffer", name: "Ryo Ueda" },
+
+        { role: "Art Director", name: "SUI (SKID)" },
+
+        { role: "Stylist", name: "Sho Sasaki" },
+        { role: "Hair", name: "Shoya Nakano" },
+        { role: "Special Effects Make-Up", name: "NOBU" },
+
+        { role: "Production Manager", name: "Kei Ukemori" },
+        { role: "Production Manager", name: "mikaze" },
+
+        { role: "Producer", name: "Matias Maehama (SKID)" }
+    ]
   },
   madras_night_part2: {
     title: "MADRAS NIGHT PART 2",
     video: "https://www.youtube.com/embed/joPPncT8z14?autoplay=1&rel=0&enablejsapi=1",
-    credits: []
+    credits: [
+        { role: "Director", name: "Hideto Hotta" },
+
+        { role: "Cinematographer", name: "Ryosuke Sato (SKID)" },
+        { role: "Assistant Camera", name: "Yuuki Maxima" },
+        { role: "Drone Operator", name: "Daisuke Maeda" },
+
+        { role: "Gaffer", name: "Makito Kaji" },
+        { role: "Lighting Assistant", name: "kiron Omura" },
+        { role: "Lighting Assistant", name: "Yuu Urata" },
+
+        { role: "Prop", name: "SUI (SKID)" },
+
+        { role: "Special Effects Make-Up", name: "Una Abe" },
+        { role: "Special Effects Make-Up Assistant", name: "horosco" },
+
+        { role: "Photographer", name: "Jun Yokoyama" },
+
+        { role: "Editor", name: "Hideto Hotta" },
+        { role: "Colorist", name: "Yoshiyuki Nishida" },
+        { role: "Compositing", name: "Una Abe" },
+
+        { role: "Title Design", name: "mad O (SKID)" },
+
+        { role: "Production Manager", name: "Shotaro Ito (SKID)" },
+        { role: "Production Manager", name: "Tsubasa Seki" },
+        { role: "Production Manager", name: "sayounaraartist" },
+
+        { role: "Producer", name: "Matias Maehama (SKID)" },
+
+        { role: "Special Thanks", name: "Takako Noel" }
+    ]
   },
   ginga: {
     title: "銀河",
     video: "https://www.youtube.com/embed/kIMNUqJusnQ?autoplay=1&rel=0&enablejsapi=1",
-    credits: []
-  },
-  silent_city: {
-    title: "Silent City",
-    video: "https://www.youtube.com/embed/vOEBTZCBKDM?autoplay=1&rel=0&enablejsapi=1",
     credits: [
-      { role: "Director", name: "Oliver Bennett" },
-      { role: "Producer", name: "Sienna Ward" },
-      { role: "Director of Photography", name: "Carter Long" },
-      { role: "Camera Operator", name: "Ruby Powell" },
-      { role: "Gaffer", name: "Hunter Adams" },
-      { role: "Editor", name: "Leah Russell" },
-      { role: "Colorist", name: "Ian Hayes" },
-      { role: "Sound Designer", name: "Piper James" },
-      { role: "Production Designer", name: "Miles Patterson" },
-      { role: "VFX Supervisor", name: "Quinn Howard" }
+        { role: "Director", name: "TAIGA (SKID)" },
+        { role: "Producer", name: "Matias Maehama (SKID)" },
+
+        { role: "Director of Photography", name: "Ryosuke Sato" },
+        { role: "First Assistant Camera", name: "Jennifer Keruzore" },
+        { role: "Second Assistant Camera", name: "Javier Sauve" },
+        { role: "Second Assistant Camera", name: "Kao Fukumoto" },
+        { role: "Drone Operator", name: "Takanari Kawasumi" },
+
+        { role: "Gaffer", name: "Wataru Sugimura" },
+        { role: "Best Boy Electric", name: "Kentaro Hirabayashi" },
+
+        { role: "Set Designer", name: "Mugiho Sasaki" },
+        { role: "Assistant Set Designer", name: "Taiki Yokote" },
+
+        { role: "Stylist", name: "Kazuma Asami (SKID)" },
+        { role: "Hair & Make-Up", name: "Kei Kouda (SKID)" },
+
+        { role: "Flower Girl", name: "Kyoko Maehama" },
+        { role: "Flower Girl", name: "Yuki Nishimoto" },
+
+        { role: "Production Manager", name: "YOICHIRO (SKID)" },
+        { role: "Production Assistant", name: "Ryo Suda" },
+        { role: "Production Assistant", name: "Kei Ukemori" },
+
+        { role: "Executive Producer", name: "Naoyuki Chikatani (To U Music Inc.)" },
+
+        { role: "Special Thanks", name: "Ryutaro Kobayashi" },
+
+        { role: "Production", name: "SKID" }
     ]
-  }
+  },
 };
